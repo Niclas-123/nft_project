3297ba0be67e143daacacdb1a234b24d6e0f67a3
Niclas Collmer <niclascollmer@googlemail.com>
2021-12-18 16:21:42 +0100

    Alles neu

--- Project.R
+++ Project.R
@@ -1,23 +1,67 @@
 library(tidyverse)
 library(rvest)
+library(stringr)
+library(RSelenium)
+
+rD <- rs
+Driver(browser="firefox", port=4545L, verbose=F)
+remDr <- rD[["client"]]
 
 nft_page <- read_html("https://opensea.io/collection/degenerate-regenerate")
+single_nft_page <- read_html("https://opensea.io/assets/0x7828c811636ccf051993c1ec3157b0b732e55b23/3182")
+
 
 number_of_items <- nft_page %>% html_elements(".gjwKJf") %>% html_text()
 
- K_in_number <- function(number) {
+item_name <- nft_page %>% html_elements(".AssetCardFooter--name") %>% html_text()
+price <- nft_page %>% html_elements(".Price--amount") %>% html_text() %>% as.numeric
+likes <- nft_page %>% html_elements(".kDRydb") %>% html_text()
+image_link <- nft_page %>% html_elements(".Image--image") %>% html_attr("src")
+
+views <- single_nft_page %>% html_elements(".gdHQWX") %>% html_text()
+title <- single_nft_page %>% html_elements(".item--title") %>% html_text()
+fiat <- single_nft_page %>% html_elements(".Price--fiat-amount-secondary") %>% html_text()
+price <- single_nft_page %>% html_elements(".Price--amount") %>% html_text()
+
+
+
+K_in_number <- function(number) {
   if(grepl("K$", number)) {
     number <- gsub("K$", "00", number) %>% gsub("\\.", "", .) 
   }
   return(as.numeric(number))
-}
+ }
+ 
+views_extractor <- function(string) {
+   extracted_view <- string %>% str_extract("[0-9]+")
+   return(as.numeric(extracted_view))
+ }
+ 
+price_extractor <- function(string) {
+   extracted_price <- string %>% str_extract("[0-9]+.[0-9]+")
+   return(as.numeric(extracted_price))
+ }
 
-prices <- nft_page %>% html_elements(".AssetCardFooter--price-amout") %>% html_text()
-number_of_items
 
-header <- tibble(
+packages <- tibble(
   items = K_in_number(number_of_items[1]),
   owners = K_in_number(number_of_items[2]),
   floor_price = number_of_items[3],
   volume_traded = number_of_items[4]
 )
+
+
+nfts_package <- tibble(
+  id = item_name,
+  Price = price,
+  Likes = likes
+) %>% separate(id, c("package", "ID"), sep = " #")
+
+
+single_nft <- tibble(
+  Title = title[1],
+  Views = views_extractor(views[1]),
+  Favourites = views_extractor(views[2]),
+  Price_ETH = price[1],
+  Price_in_dollar = price_extractor(fiat[1]),
+) %>% separate(Title, c("package", "ID"), sep = " #")
