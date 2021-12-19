99e65acb968e729ea2a7b0f7653188ad5878b0f5
Niclas Collmer <niclascollmer@googlemail.com>
2021-12-19 14:08:35 +0100

    Fertige Funktion implementiert

--- Project.R
+++ Project.R
@@ -4,50 +4,18 @@ library(rvest)
 library(stringr)
 library(RSelenium)
 
-
-rD <- rsDriver(browser="firefox", port=4541L, verbose=F)
+#open Selenium Browser
+rD <- rsDriver(browser="firefox", port=4547L, verbose=F)
 remDr <- rD[["client"]]
 
-links <- c()
-old_links <- "platzhalter"
+#rD[["server"]]$stop()
 
-
-remDr$navigate("https://opensea.io/collection/getijde-by-bart-simons")
-i <- 0
-while(TRUE){
-  remDr$executeScript(paste("scroll(0,",i*5000,");"))
-  Sys.sleep(4)
-  page_source<-remDr$getPageSource()
-  nft_page <- read_html(page_source[[1]])
-  new_links <- nft_page %>% html_elements(".ekTmzq.Asset--anchor") %>% html_attr("href")
-  print(new_links)
-  if(all(new_links == old_links)) {break()}
-  links <- c(links, new_links)
-  old_links <- new_links
-  i <- i +1
-}
-
-
-read_html(page_source[[1]]) %>% html_nodes(".gjwKJf") %>% html_text()
-
-#load Pages
-nft_page <- read_html(page_source[[1]])
-single_nft_page <- read_html(page_source[[1]])
-
-#load NFT-collection DATA
-number_of_items <- nft_page %>% html_elements(".gjwKJf") %>% html_text()
-
-#load NFT-collection-Item DATA
-item_name <- nft_page %>% html_elements(".AssetCardFooter--name") %>% html_text()
-# collection_view_price <- nft_page %>% html_elements(".Price--amount") %>% html_text() %>% as.numeric
-likes <- nft_page %>% html_elements(".kDRydb") %>% html_text()
-link <- nft_page %>% html_elements(".ekTmzq.Asset--anchor") %>% html_attr("href")
-
-#load single NFT view DATA
-views <- single_nft_page %>% html_elements(".gdHQWX") %>% html_text()
-title <- single_nft_page %>% html_elements(".item--title") %>% html_text()
-fiat <- single_nft_page %>% html_elements(".Price--fiat-amount-secondary") %>% html_text()
-price <- single_nft_page %>% html_elements(".Price--amount") %>% html_text()
+list_of_collections <- c(
+  "https://opensea.io/collection/deafbeef",
+ "https://opensea.io/collection/rekt-news-hacks-hopium",
+ "https://opensea.io/collection/illuvium",
+ "https://opensea.io/collection/pop-wonder-world"
+)
 
 
 #Functions
@@ -56,38 +24,117 @@ K_in_number <- function(number) {
     number <- gsub("K$", "00", number) %>% gsub("\\.", "", .) 
   }
   return(as.numeric(number))
- }
- 
+}
+
 views_extractor <- function(string) {
-   extracted_view <- string %>% str_extract("[0-9]+")
-   return(as.numeric(extracted_view))
- }
- 
+  extracted_view <- string %>% str_extract("[0-9]+")
+  return(as.numeric(extracted_view))
+}
+
 price_extractor <- function(string) {
-   extracted_price <- string %>% str_extract("[0-9]+.[0-9]+")
-   return(as.numeric(extracted_price))
- }
+  extracted_price <- string %>% str_extract("[0-9,.]+") %>% gsub(",", "", .) %>% as.numeric()
+  return(as.numeric(extracted_price))
+}
 
-#Tables
-packages <- tibble(
-  items = K_in_number(number_of_items[1]),
-  owners = K_in_number(number_of_items[2]),
-  floor_price = number_of_items[3],
-  volume_traded = number_of_items[4]
-)
+#Funktion um einzelne NFT Seite zu scannen
+
+get_single_nft <- function(link){
+  single_nft_page <- read_html(paste0("https://opensea.io", link))
+  
+  views <- single_nft_page %>% html_elements(".gdHQWX") %>% html_text()
+  title <- single_nft_page %>% html_elements(".item--title") %>% html_text()
+  fiat <- single_nft_page %>% html_elements(".Price--fiat-amount-secondary") %>% html_text()
+  price <- single_nft_page %>% html_elements(".Price--amount") %>% html_text()
+  
+  single_nft <- tibble(
+    Title = title[1],
+    Views = views_extractor(views[1]),
+    Favourites = views_extractor(views[2]),
+    Price_ETH = price[1],
+    Price_in_dollar = price_extractor(fiat[1]),
+    Link = link
+  )
+  
+  return(single_nft)
+}
+
+get_list_of_nfts <- function(collection_link, remDr) {
+  cat("initialisiere Server\n")
+  remDr$navigate(collection_link)
+  page_source <- remDr$getPageSource()    
+  nft_page <- read_html(page_source[[1]])
+  
+    #load NFT-collection DATA
+    number_of_items <- nft_page %>% html_nodes(".gjwKJf") %>% html_text()
+    
+    cat("Komprimiere Package Information\n")
+    packages <- tibble(
+      items = K_in_number(number_of_items[1]),
+      owners = K_in_number(number_of_items[2]),
+      floor_price = number_of_items[3],
+      volume_traded = number_of_items[4]
+    )
+  
+    #click button for small NFT view
+    cat("Clicke Button für kleine Ansicht\n")
+    small_view <- remDr$findElement(using = 'css selector', '.bnWGYU')
+    small_view$clickElement()
+    
+    i <- 0
+    links <- c()
+    item_names <- c()
+    likes <- c()
+    
+    old_links <- "platzhalter"
+    old_item_names <- "platzhalter"
+    old_likes <- "platzhalter"
+    
+    cat("Schrolle durch die NFTs\n")
+    
+    while(TRUE){
+      remDr$executeScript(paste("scroll(0,",i*1500,");"))
+      Sys.sleep(4)
+      page_source<-remDr$getPageSource()
+      nft_page <- read_html(page_source[[1]])
+      
+      new_links <- nft_page %>% html_elements(".ekTmzq.Asset--anchor") %>% html_attr("href")
+      new_item_names <- nft_page %>% html_elements(".AssetCardFooter--name") %>% html_text()
+      new_likes <- nft_page %>% html_elements(".kDRydb") %>% html_text()
+      
+      if(all(new_links == old_links)) {break()}
+        links <- c(links, new_links)
+        item_names <- c(item_names, new_item_names)
+        likes <- c(likes, new_likes)
+        
+        old_item_names <- new_item_names
+        old_likes <- new_likes
+        old_links <- new_links
+        i <- i +1
+    } 
+    
+  cat("Komprimiere Liste der NFTs aus Collection Ansicht\n")  
+  nfts_package <- tibble(
+      id = item_names,
+      Likes = likes,
+      Link = links,
+    ) %>% distinct
+    
+    
+  
+  cat("Erstelle Liste der NFT's aus single NFT View\n")
+  all_nfts <- map_df(links, get_single_nft)
+  
+  cat("Füge Listen zusammen\n")
+  ultimate_nft_list <- full_join(nfts_package, all_nfts, by = "Link") %>% distinct()
+  
+  cat("Fertig\n")
+  return(list(package = packages, nfts = ultimate_nft_list, a = nfts_package, b = all_nfts))
+}
+
+x <- get_list_of_nfts(list_of_collections[2], remDr = remDr)
+
+data <- map(list_of_collections, safely(get_list_of_nfts))
 
 
-nfts_package <- tibble(
-  id = item_name,
-  Likes = likes,
-  Link = link
-) %>% separate(id, c("package", "ID"), sep = " #")
-
-
-single_nft <- tibble(
-  Title = title[1],
-  Views = views_extractor(views[1]),
-  Favourites = views_extractor(views[2]),
-  Price_ETH = price[1],
-  Price_in_dollar = price_extractor(fiat[1]),
-) %>% separate(Title, c("package", "ID"), sep = " #")
+# single_nft <- get_single_nft(links[1])
+# all_nfts <- map_df(links, get_single_nft)
