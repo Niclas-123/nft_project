e1582f1ec5088bd3c961bfab4dfc74d8ef6c9440
Niclas Collmer <niclascollmer@googlemail.com>
2021-12-19 10:05:50 +0100

    While Loop zum Link Scraping eingefügt

--- Project.R
+++ Project.R
@@ -5,32 +5,43 @@ library(stringr)
 library(RSelenium)
 
 
-rD <- rsDriver(browser="firefox", port=4545L, verbose=F)
+rD <- rsDriver(browser="firefox", port=4541L, verbose=F)
 remDr <- rD[["client"]]
 
-remDr$navigate("https://opensea.io/collection/degenerate-regenerate")
-for(i in 1:100){
-  remDr$executeScript(paste("scroll(0,",i*10000,");"))
-  Sys.sleep(3)
+links <- c()
+old_links <- "platzhalter"
+
+
+remDr$navigate("https://opensea.io/collection/getijde-by-bart-simons")
+i <- 0
+while(TRUE){
+  remDr$executeScript(paste("scroll(0,",i*5000,");"))
+  Sys.sleep(4)
+  page_source<-remDr$getPageSource()
+  nft_page <- read_html(page_source[[1]])
+  new_links <- nft_page %>% html_elements(".ekTmzq.Asset--anchor") %>% html_attr("href")
+  print(new_links)
+  if(all(new_links == old_links)) {break()}
+  links <- c(links, new_links)
+  old_links <- new_links
+  i <- i +1
 }
 
-page_source<-remDr$getPageSource()
 
-html(page_source[[1]]) %>% html_nodes(".gjwKJf") %>%
-  html_text()
+read_html(page_source[[1]]) %>% html_nodes(".gjwKJf") %>% html_text()
 
 #load Pages
-nft_page <- read_html("https://opensea.io/collection/degenerate-regenerate")
-single_nft_page <- read_html("https://opensea.io/assets/0x7828c811636ccf051993c1ec3157b0b732e55b23/3182")
+nft_page <- read_html(page_source[[1]])
+single_nft_page <- read_html(page_source[[1]])
 
 #load NFT-collection DATA
 number_of_items <- nft_page %>% html_elements(".gjwKJf") %>% html_text()
 
 #load NFT-collection-Item DATA
 item_name <- nft_page %>% html_elements(".AssetCardFooter--name") %>% html_text()
-collection_view_price <- nft_page %>% html_elements(".Price--amount") %>% html_text() %>% as.numeric
+# collection_view_price <- nft_page %>% html_elements(".Price--amount") %>% html_text() %>% as.numeric
 likes <- nft_page %>% html_elements(".kDRydb") %>% html_text()
-image_link <- nft_page %>% html_elements(".Image--image") %>% html_attr("src")
+link <- nft_page %>% html_elements(".ekTmzq.Asset--anchor") %>% html_attr("href")
 
 #load single NFT view DATA
 views <- single_nft_page %>% html_elements(".gdHQWX") %>% html_text()
@@ -68,8 +79,8 @@ packages <- tibble(
 
 nfts_package <- tibble(
   id = item_name,
-  Price = collection_view_price,
-  Likes = likes
+  Likes = likes,
+  Link = link
 ) %>% separate(id, c("package", "ID"), sep = " #")
 
 
